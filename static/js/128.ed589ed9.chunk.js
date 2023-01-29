1445c7f001163405aea03a2e4e60eba4a508ba8f
pbelyanov <p.belyanov@yahoo.com>
2023-01-29 20:54:12 +0200

    Updates

---
+++ static/js/128.ed589ed9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirst_react_app=self.webpackChunkfirst_react_app||[]).push([[128],{128:function(e,t,a){a.r(t),t.default={}}}]);
+//# sourceMappingURL=128.ed589ed9.chunk.js.map
---
+++ static/js/96.fca8212b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfirst_react_app=self.webpackChunkfirst_react_app||[]).push([[96],{96:function(e,t,a){a.r(t),t.default={}}}]);
+//# sourceMappingURL=96.fca8212b.chunk.js.map
