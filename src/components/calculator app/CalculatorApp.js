860d572b5448e6010ab5ba8e84c24ca450dea329
pbelyanov <p.belyanov@yahoo.com>
2023-02-27 17:45:29 +0200

    Added calculator logic

--- src/components/calculator app/CalculatorApp.js
+++ src/components/calculator app/CalculatorApp.js
@@ -3,20 +3,91 @@ import { useState } from "react";
 import("./calcStyle.css");
 
 export function CalculatorApp() {
-  const [expresson, changeExpression] = useState("");
+  const [firstNum, changeFirstNum] = useState("");
+  const [operand, changeOperand] = useState(null);
+  const [secondNum, changeSecondNum] = useState("");
+
   const nums = [];
-  for (let i = 0; i <= 9; i++) {
+  for (let i = 1; i <= 9; i++) {
     nums.push(i);
   }
-  console.log(nums);
+  nums.push("0");
+  nums.push(".");
+  nums.push("=");
   const operands = ["+", "-", "/", "*"];
+
+  function numClicked(e) {
+    if (!operand) {
+      const currentResult = firstNum + e.target.innerHTML;
+      changeFirstNum(currentResult);
+    } else {
+      const currentResult = secondNum + e.target.innerHTML;
+      changeSecondNum(currentResult);
+    }
+  }
+
+  function operandClicked(e) {
+    if (!operand) {
+      changeOperand(e.target.innerHTML);
+    } else {
+      return;
+    }
+  }
+
+  function clearEverything(e) {
+    changeFirstNum("");
+    changeOperand(null);
+    changeSecondNum("");
+  }
+
+  function getResult() {
+    let result = 0;
+    switch (operand) {
+      case "+":
+        result = Number(firstNum) + Number(secondNum);
+        break;
+      case "-":
+        result = Number(firstNum) - Number(secondNum);
+        break;
+      case "/":
+        result = Number(firstNum) / Number(secondNum);
+        break;
+      case "*":
+        result = Number(firstNum) * Number(secondNum);
+        break;
+    }
+    changeFirstNum(result);
+    changeOperand(null);
+    changeSecondNum("");
+  }
 
   return (
     <div id="calcPage">
-      <div className="nums">
-        {nums.map((x) => (
-          <a>{x}</a>
-        ))}
+      <div className="calcApp">
+        <div className="calcScreen">
+          <div id="topSide">{operand ? `${firstNum} ${operand}` : null}</div>
+          <div id="botSide">{!operand ? firstNum : secondNum}</div>
+        </div>
+        <div className="specialButtons">
+          <button id="allClear" onClick={clearEverything}>
+            AC
+          </button>
+          <button id="equals" onClick={getResult}>
+            =
+          </button>
+        </div>
+        <div className="calcButtons">
+          <div className="nums">
+            {nums.map((x) => (
+              <button onClick={numClicked}>{x}</button>
+            ))}
+          </div>
+          <div className="operands">
+            {operands.map((x) => (
+              <button onClick={operandClicked}>{x}</button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
